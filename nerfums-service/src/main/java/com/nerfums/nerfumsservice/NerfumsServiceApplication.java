279a1c63a3e4b7c8db206e0d3049fedfcd27893e
Will Jackins <will.jackins@moodys.com>
2019-12-11 11:15:22 -0500

    Added initial code for nerfums webservice.

---
+++ nerfums-service/src/main/java/com/nerfums/nerfumsservice/NerfumsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nerfums.nerfumsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerfumsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerfumsServiceApplication.class, args);
+	}
+
+}
