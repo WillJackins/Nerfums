a5b6414c264a610cd73f146b0b0abdfcbf544ccc
WillJackins <57357895+WillJackins@users.noreply.github.com>
2020-01-14 16:05:35 -0500

    Added an about page. Also added a Page Navigation bar. (#37)

--- nerfums-app/src/app/app-routing.module.ts
+++ nerfums-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AboutPageComponent} from './about-page/about-page.component';
+import {ContractManagerComponent} from './contract-manager/contract-manager.component';
+import {ContractListComponent} from './contract-list/contract-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/about', pathMatch: 'full'},
+  {path: 'about', component: AboutPageComponent},
+  {path: 'contractManager', component: ContractManagerComponent},
+  {path: 'contractList', component: ContractListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- nerfums-app/src/app/app.module.ts
+++ nerfums-app/src/app/app.module.ts
@@ -1,30 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
-import { HttpClientModule } from '@angular/common/http';
-import { ContractListComponent } from './contract-list/contract-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ContractListComponent} from './contract-list/contract-list.component';
 
-import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContractItemComponent } from './contract-item/contract-item.component';
+import {MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ContractItemComponent} from './contract-item/contract-item.component';
 import {MatGridListModule} from '@angular/material/grid-list';
-import { ContractCreateComponent } from './contract-create/contract-create.component';
+import {ContractCreateComponent} from './contract-create/contract-create.component';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {MatSelectModule} from '@angular/material/select';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatExpansionModule} from '@angular/material/expansion';
-import { ContractManagerComponent } from './contract-manager/contract-manager.component';
+import {ContractManagerComponent} from './contract-manager/contract-manager.component';
 import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {ContractActiveListComponent, ContractDeleteDialogComponent} from './contract-active-list/contract-active-list.component';
-import {ContractDashboardComponent } from './contract-dashboard/contract-dashboard.component';
+import {ContractDashboardComponent} from './contract-dashboard/contract-dashboard.component';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatIconModule} from '@angular/material/icon';
-import { ContractCompleteComponent } from './contract-complete/contract-complete.component';
+import {ContractCompleteComponent} from './contract-complete/contract-complete.component';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {AboutPageComponent} from './about-page/about-page.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {_MatMenuDirectivesModule, MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -36,7 +39,8 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     ContractActiveListComponent,
     ContractDashboardComponent,
     ContractDeleteDialogComponent,
-    ContractCompleteComponent
+    ContractCompleteComponent,
+    AboutPageComponent
   ],
   entryComponents: [
     ContractCreateComponent,
@@ -62,7 +66,10 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     MatExpansionModule,
     MatDialogModule,
     MatIconModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatTabsModule,
+    _MatMenuDirectivesModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
