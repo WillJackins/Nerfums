a5b9c52b018b5793cb25bcae188ef0b214241579
Will Jackins <will.jackins@moodys.com>
2020-02-02 01:21:45 -0500

    Updated user menu with username, money, and logout button.

--- nerfums-app/src/app/nerfums.service.ts
+++ nerfums-app/src/app/nerfums.service.ts
@@ -7,6 +7,7 @@ import {Modifier} from '../model/Modifier';
 import {Session} from "../model/Session";
 import {catchError, map} from "rxjs/operators";
 import {Register} from "../model/Register";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -18,12 +19,9 @@ export class NerfumsService {
   private currentSessionSubject: BehaviorSubject<Session>;
   public currentSession: Observable<Session>;
 
-  constructor(private http: HttpClient) {
+  constructor(private router: Router, private http: HttpClient) {
     this.currentSessionSubject = new BehaviorSubject<Session>(JSON.parse(localStorage.getItem('currentSession')));
     this.currentSession = this.currentSessionSubject.asObservable();
-
-    //TODO REMOVE
-    this.logout();
   }
 
   public get currentSessionValue(): Session {
@@ -65,6 +63,7 @@ export class NerfumsService {
   logout() {
     localStorage.removeItem('currentSession');
     this.currentSessionSubject.next(null);
+    this.router.navigate(['/about'])
   }
 
   getAllActiveContracts(activeContracts: boolean): Observable<Array<Contract>> {
